fd841266a0334a42f10b6b30bb96ef2d33e00c45
Vijaye Raji <vijaye@statsig.com>
2022-04-28 22:36:59 -0700

    AAAAAAARRRRRRRGGGGGGGHHHHHHH

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
         AB testing with Statsig
       </Text>
       <Text className="mb-4">
-        In this demo we use Statsig's Edge SDK to pull experiment variant and
+        In this demo we use Statsig&apos;s Edge SDK to pull experiment variant and
         show the resulting allocation.  As you change the user-id you will see
         the variant assignment change, but as long as the user-id is constant,
         the variant assignment will be sticky.
